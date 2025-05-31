a801ce96134a99cfd3f4afd658ef9c70150061cd
Gary Forster <gary@garyforster.io>
2025-05-31 20:32:18 +0900

    feat: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,29 @@
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import { storybookNextJsPlugin } from '@storybook/experimental-nextjs-vite/vite-plugin';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [storybookNextJsPlugin(), storybookTest()],
+  publicDir: './public',
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      headless: true,
+      screenshotFailures: false,
+      instances: [
+        {
+          browser: 'chromium',
+        },
+      ],
+    },
+    isolate: false,
+    setupFiles: ['./.storybook/vitest.setup.ts'],
+    coverage: {
+      all: true,
+      include: ['{app,lib,components}/**/*'],
+      exclude: [...coverageConfigDefaults.exclude, '**/*.{stories,mock}.*'],
+      provider: 'istanbul',
+    },
+  },
+});
