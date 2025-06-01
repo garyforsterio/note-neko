2e73b3974f09a6cee0cf6a904e830af20cd10f7a
Gary Forster <gary@garyforster.io>
2025-06-01 23:23:17 +0900

    feat: configure accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const createPrismaClient = () =>
   new PrismaClient({
@@ -6,12 +7,12 @@ const createPrismaClient = () =>
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
         : ['error'],
-  });
+  }).$extends(withAccelerate());
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
