c0d76a4906e93a1c1971746e6c71398296001cd0
Gary Forster <gary@garyforster.io>
2025-06-17 23:37:33 +0200

    feat: add sentry

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,7 +54,7 @@ export const config = {
 		 * - _next/image (image optimization files)
 		 * - favicon.ico (favicon file)
 		 */
-		"/((?!api|images|manifest.json|_next/static|_next/image|favicon.ico).*)",
+		"/((?!api|images|monitoring|manifest.json|_next/static|_next/image|favicon.ico).*)",
 	],
 	runtime: "nodejs",
 };
