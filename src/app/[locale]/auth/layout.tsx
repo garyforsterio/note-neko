42c653e6339fb7c51fadb9cd08b251c7e045bb5e
Gary Forster <gary@garyforster.io>
2025-06-29 18:02:02 +0900

    chore: remove auth title

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -9,14 +9,7 @@ export default async function AuthLayout({
 
 	return (
 		<div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
-			<div className="max-w-md w-full space-y-8">
-				<div>
-					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-						{t("auth.title")}
-					</h2>
-				</div>
-				{children}
-			</div>
+			<div className="max-w-md w-full space-y-8">{children}</div>
 		</div>
 	);
 }
