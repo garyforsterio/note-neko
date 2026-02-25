a2819931e4cf783c935778d9dce2b6c0c4f69178
Gary Forster <gary@garyforster.io>
2026-02-26 00:29:41 +0900

    feat: add terraform base config

---
+++ terraform/ephemeral/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "life-tracker-459800"
+  region  = "asia-northeast1"
+}
